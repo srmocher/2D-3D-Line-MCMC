1c286ebb752c7410e00c42c895e7b5e7b11dd280
srmocher <srmocher@email.arizona.edu>
2017-12-11 23:27:54 -0700

    Added comments

--- mhsampling.py
+++ mhsampling.py
@@ -2,16 +2,13 @@ import numpy as np
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 
-# common camera matrix for all scenarios
-
-
 
 def get_2d_projection_of_3d_point(camera_matrix,point3d):
     '''
 
-    :param camera_matrix:
-    :param point3d:
-    :return:
+    :param camera_matrix: camera matrix belonging to the camera from which 3D point is viewed
+    :param point3d: the 3D point whose 2D projection is required
+    :return: 2D projection (x,y) of point3D
     '''
     projection = camera_matrix.dot(np.append(point3d,1))
     point_2d = projection/projection[2]
@@ -20,13 +17,12 @@ def get_2d_projection_of_3d_point(camera_matrix,point3d):
 def get_log_likelihood(r_s,q_i,q_f,t_ratios,sigma_2d):
     '''
 
-    :param r_s:
-    :param q_i:
-    :param q_f:
-    :param t_ratios:
-    :param sigma_2d:
-    :param r_prime:
-    :return:
+    :param r_s: 2D points
+    :param q_i: initial endpoint of the 2D projection
+    :param q_f: other endpoint of the 2D projection
+    :param t_ratios: ratios for point interpolations along the line
+    :param sigma_2d: 2x2 covariance matrix
+    :return: array of likelihood distributions on logarithmic scale for each point
     '''
     num_points = r_s.shape[0]
     q_s = np.zeros(shape=(num_points, 2))
@@ -43,31 +39,31 @@ def get_log_likelihood(r_s,q_i,q_f,t_ratios,sigma_2d):
 def get_prior(p_i,p_f,mu_3d,sigma_3d):
     '''
 
-    :param p_i:
-    :param p_f:
-    :param mu_3d:
-    :param sigma_3d:
-    :return:
+    :param p_i: point p_i
+    :param p_f: point p_f
+    :param mu_3d: 3x1 mean vector
+    :param sigma_3d: 3x3 covariance matrix
+    :return: normal pdf in log scale with parameters from the function
     '''
     return multivariate_normal.logpdf([p_i,p_f],mu_3d,sigma_3d)
 
 def proposal(previous,sigma):
     '''
 
-    :param previous:
-    :param sigma:
-    :return:
+    :param previous: previous sample so that distribution can be centered around that
+    :param sigma: covariance matrix
+    :return: random sample from normal distribution denoting the next state in the Markov chain
     '''
     return multivariate_normal.rvs(previous,sigma)
 
 def plot_parameter(x,y,x_label,y_label,title):
     '''
 
-    :param x:
-    :param y:
-    :param x_label:
-    :param y_label:
-    :param title:
+    :param x: x-axis values
+    :param y: y-axis values
+    :param x_label: x-axis label
+    :param y_label: y-axis label
+    :param title: Title of the plot
     :return:
     '''
     figure = plt.figure()
@@ -112,16 +108,20 @@ def mh_sample_posterior_3d(camera_matrix,t_ratios,r_points,sigma_2d,mu_3d,sigma_
        q_f_prime = get_2d_projection_of_3d_point(new_camera_matrix,p_f)
        log_likelihood = log_likelihood + get_log_likelihood(r_prime,q_i_prime,q_f_prime,t_ratios,sigma_2d)
 
-
+   # compute posterior
    log_posterior = np.sum(log_likelihood) + np.sum(log_prior)
+
+   # get next sample from proposal distribution centered around current sample
    p_i_next = proposal(p_i,sigma_3d)
    p_f_next = proposal(p_f,sigma_3d)
 
+   # compute likelihood for proposed samples
    q_i_next = get_2d_projection_of_3d_point(camera_matrix,p_i_next)
    q_f_next = get_2d_projection_of_3d_point(camera_matrix,p_f_next)
 
    log_likelihood_next = get_log_likelihood(r_points,q_i_next,q_f_next,t_ratios,sigma_2d)
 
+   # if camera 2 data is to be included, then add it to log likelihood
    if r_prime is not None:
        q_i_prime_next = get_2d_projection_of_3d_point(new_camera_matrix,p_i_next)
        q_f_prime_next = get_2d_projection_of_3d_point(new_camera_matrix,p_f_next)
@@ -129,16 +129,23 @@ def mh_sample_posterior_3d(camera_matrix,t_ratios,r_points,sigma_2d,mu_3d,sigma_
 
    log_prior_next = get_prior(p_i_next,p_f_next,mu_3d,sigma_3d)
 
+   # compute posterior with new sample
    log_posterior_next = np.sum(log_likelihood_next) + np.sum(log_prior_next)
 
+   # compute log of acceptance ratio
    log_r = log_posterior_next - log_posterior
+
+   # accept
    if log_r >=0:
        return p_i_next,p_f_next,log_posterior_next,True
    else:
        u = np.random.uniform()
+
+       # accept
        if log_r >= np.log(u):
            return p_i_next,p_f_next,log_posterior_next,True
        else:
+           # reject
            return p_i,p_f,log_posterior,False
 
 
--- tasks.py
+++ tasks.py
@@ -12,32 +12,43 @@ num_iterations = 50000
 def task2(r_points,t_ratios,r_prime,new_camera_matrix=None):
     '''
 
-    :param r_points:
-    :param t_ratios:
-    :param r_prime:
-    :param new_camera_matrix:
-    :return:
+    :param r_points: points corresponding to camera 1 in points_2d_camera_1.csv
+    :param t_ratios: ratios corresponding to points, defined in inputs.csv
+    :param r_prime: points corresponding to camera 2, in points_2d_camera_2.csv.
+    :param new_camera_matrix: camera 2 matrix. Required in task 5.
+    :return: accepted samples, posterior probabilties
     '''
+
+    # define parameters for distributions
     sigma_3d = 6 * np.identity(3)
     mu_3d = np.array([0, 0, 4]).T
     sigma_2d = (0.05) * (0.05) * np.identity(2)
     pi_points = []
     pf_points = []
 
+    # sample initial p_i, p_f from prior
     p_i_current, p_f_current = multivariate_normal.rvs(mu_3d, sigma_3d, size=2)
+
+    # storing probabilities to compute MAP estimate
     posterior_probs = np.empty(shape=(num_iterations,1))
     for i in range(0, num_iterations):
-        if i%100 == 0:
-            print(str(i)+" iterations complete")
+        # statement to print progress
+        # if i%100 == 0:
+        #     print(str(i)+" iterations complete")
+
+        # sample from MH. Can accept or reject. If reject returns current sample as new sample.
         new_pi, new_pf, posterior, accepted = mh_sample_posterior_3d(camera_matrix,t_ratios, r_points, sigma_2d, mu_3d, sigma_3d,
                                                                      p_i_current, p_f_current,r_prime,new_camera_matrix)
         posterior_probs[i]=posterior
         pi_points.append(new_pi)
         pf_points.append(new_pf)
+
+        # if accepted, update current values in MC to new values
         if accepted:
             p_i_current = new_pi
             p_f_current = new_pf
 
+    # plotting number of samples with each parameter
     indices = np.linspace(0, num_iterations, num_iterations)
     p_i_x = np.array(pi_points)[:, 0]
     p_i_y = np.array(pi_points)[:, 1]
@@ -59,25 +70,35 @@ def task2(r_points,t_ratios,r_prime,new_camera_matrix=None):
 def task3(r_points,pi_points,pf_points,posterior_probs):
     '''
 
-    :param r_points:
-    :param pi_points:
-    :param pf_points:
-    :param posterior_probs:
+    :param r_points: points corresponding to camera 1. Defined in points_2d_camera_1.csv
+    :param pi_points: List of accepted pi values after running MH
+    :param pf_points: List of accepted pf values after running MH
+    :param posterior_probs: Array of posterior probabilities
     :return:
     '''
+
+    # Get index corresponding to MAP value
     max_index_posterior = np.argmax(posterior_probs)
+
+    # Get points corresponding to MAP value
     p_i_map_estimate = pi_points[max_index_posterior]
     p_f_map_estimate = pf_points[max_index_posterior]
     print(r'MAP estimate obtained in task3 for $p_i$ is '+str(p_i_map_estimate))
-    print(r'MAP estimate obtained in task3 for $p_i$ is '+str(p_f_map_estimate))
+    print(r'MAP estimate obtained in task3 for $p_f$ is '+str(p_f_map_estimate))
 
-
+    # project MAP points to 2D space
     q_i_map_estimate = get_2d_projection_of_3d_point(camera_matrix,p_i_map_estimate)
     q_f_map_estimate = get_2d_projection_of_3d_point(camera_matrix,p_f_map_estimate)
+
+    # camera 1 data
     r_x = r_points[:, 0]
     r_y = r_points[:, 1]
+
+    # plot camera 1 data with MAP estimate line
     figure = plt.figure()
     figure.suptitle('2-D projection of MAP estimate of 3D line along with points from points_2d_camera_1.csv')
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.scatter(r_x, r_y)
     q_x = np.array([q_i_map_estimate[0], q_f_map_estimate[0]])
     q_y = np.array([q_i_map_estimate[1], q_f_map_estimate[1]])
@@ -90,16 +111,21 @@ def task3(r_points,pi_points,pf_points,posterior_probs):
 def task4(max_index_posterior,pi_points,pf_points,r_prime):
     '''
 
-    :param max_index_posterior:
-    :param pi_points:
-    :param pf_points:
-    :param r_prime:
+    :param max_index_posterior: index corresponding to MAP value
+    :param pi_points: list of accepted pi points after running MH
+    :param pf_points: list of accepted pf points after running mH
+    :param r_prime: list of points corresponding to camera 2 in points_2d_camera_2.csv
     :return:
     '''
+
+    # Get MAP points obtained from task3
     p_i_map_estimate = pi_points[max_index_posterior]
     p_f_map_estimate = pf_points[max_index_posterior]
+
+    # project in 2d space using camera 2
     q_i_map_estimate = get_2d_projection_of_3d_point(new_camera_matrix,p_i_map_estimate)
     q_f_map_estimate = get_2d_projection_of_3d_point(new_camera_matrix,p_f_map_estimate)
+
 
     q_x = np.array([q_i_map_estimate[0], q_f_map_estimate[0]])
     q_y = np.array([q_i_map_estimate[1], q_f_map_estimate[1]])
@@ -107,9 +133,9 @@ def task4(max_index_posterior,pi_points,pf_points,r_prime):
     r_prime_x = r_prime[:,0]
     r_prime_y = r_prime[:,1]
 
-
+    # plot camera 2 data with projection of MAP estimate obtained with only camera 1 data
     figure = plt.figure()
-    figure.suptitle('2-D projection of MAP estimate from task 3 with camera 2 along with points from points_2d_camera_1.csv')
+    figure.suptitle('2-D projection of MAP estimate from task 3 with camera 2 along with points from points_2d_camera_2.csv')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.scatter(r_prime_x,r_prime_y)
@@ -121,28 +147,33 @@ def task4(max_index_posterior,pi_points,pf_points,r_prime):
 def task5(r_points,t_ratios,r_prime):
     '''
 
-    :param r_points:
-    :param t_ratios:
-    :param r_prime:
+    :param r_points: points corresponding to camera 1 as defined in points_2d_camera_1.csv
+    :param t_ratios: ratios corresponding to points as defined in inputs.csv
+    :param r_prime: points corresponding to camera 2 as defined in points_2d_camera_2.csv
     :return:
     '''
+    # run task but combine camera 2 data in the likelihood
     pi,pf,posterior_probs = task2(r_points,t_ratios,r_prime,new_camera_matrix)
+
+    # get new MAP estimate with combined cameras
     max_index_posterior = np.argmax(posterior_probs)
     p_i_map_estimate = pi[max_index_posterior]
     p_f_map_estimate = pf[max_index_posterior]
     print(r"MAP estimate for $p_i$ in task5 is "+str(p_i_map_estimate))
     print(r"MAP estimate for $p_f$ in task5 is "+str(p_f_map_estimate))
 
-
+    # project into 2D space using camera 1
     q_i_map = get_2d_projection_of_3d_point(camera_matrix,p_i_map_estimate)
     q_f_map = get_2d_projection_of_3d_point(camera_matrix,p_f_map_estimate)
 
+    # project into 2D space using camera 2
     q_i_map_prime = get_2d_projection_of_3d_point(new_camera_matrix,p_i_map_estimate)
     q_f_map_prime = get_2d_projection_of_3d_point(new_camera_matrix,p_f_map_estimate)
 
     q_x = np.array([q_i_map[0],q_f_map[0]])
     q_y = np.array([q_i_map[1],q_f_map[1]])
 
+    # plot camera 1 data with MAP estimate line obtained by combined cameras
     fig1 = plt.figure()
     fig1.suptitle('2-D projection of MAP estimate using camera 1 with likelihood combining both camera sources')
     plt.xlabel('x')
@@ -150,6 +181,7 @@ def task5(r_points,t_ratios,r_prime):
     plt.plot(q_x,q_y)
     plt.scatter(r_points[:,0],r_points[:,1])
 
+    # plot camera 2 data with MAP estimate line obtained by combined cameras
     fig2 = plt.figure()
     plt.xlabel('x')
     plt.ylabel('y')
@@ -162,17 +194,19 @@ def task5(r_points,t_ratios,r_prime):
     plt.show()
 
 
-
-
-
-
-
-
-# Plot parameters
+# read files
 r_points = np.genfromtxt('data/points_2d_camera_1.csv', delimiter=',')
 r_prime = np.genfromtxt('data/points_2d_camera_2.csv',delimiter=',')
 t_ratios = np.genfromtxt('data/inputs.csv', delimiter=',')
+
+# run task 2 and get accedpted samples and posterior probabilties
 pi_points,pf_points,posterior_probs = task2(r_points,t_ratios,None)
+
+# run task 3 and plot the MAP estimate using camera 1
 max_index_post = task3(r_points,pi_points,pf_points,posterior_probs)
+
+# run task 4 and plot the MAP estimate using camera 2 - bad fit!
 task4(max_index_post,pi_points,pf_points,r_prime)
+
+# run task  with combined camera data and plot the lines - good fits!
 task5(r_points,t_ratios,r_prime)
